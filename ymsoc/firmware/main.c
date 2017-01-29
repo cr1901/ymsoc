7d8c1b42197ae694aa01ec0bfbe70e72e4d5d928
William D. Jones <thor0505@comcast.net>
2017-01-29 10:22:53 -0500

    Rewrite firmware test.

--- ymsoc/firmware/main.c
+++ ymsoc/firmware/main.c
@@ -5,20 +5,14 @@
 
 int main(int argc, char * argv[])
 {
-    unsigned char i = 0;
-    //write_ym2151_addr(0x1B);
+    write_ym2151_addr(0x1B);
+    while(YM_CHECK_BUSY);
 
     while(1)
     {
-        write_ym2151_wait(0x1B, 0x40);
-        write_ym2151_wait(0x1B, 0x80);
-        //write_ym2151_data(0x40);
-        //wait_ym2151();
-        //write_ym2151_data(0x80);
-        //wait_ym2151();
-        //write_ym2151_data(0xC0);
-        //wait_ym2151();
-        //write_ym2151_data(0x00);
-        //wait_ym2151();
+        write_ym2151_data(0x40);
+        while(YM_CHECK_BUSY);
+        write_ym2151_data(0x80);
+        while(YM_CHECK_BUSY);
     }
 }
