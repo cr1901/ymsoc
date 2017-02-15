79ec93f60fa7338efb7ca25f0ada46c02147d193
William D. Jones <thor0505@comcast.net>
2017-02-15 03:04:27 -0500

    Tabs to spaces.

--- ymsoc/firmware/ymselftest/isr.c
+++ ymsoc/firmware/ymselftest/isr.c
@@ -16,56 +16,56 @@ void handle_sample(void);
 
 void isr(void)
 {
-	unsigned int irqs;
+    unsigned int irqs;
 
-	irqs = irq_pending() & irq_getmask();
+    irqs = irq_pending() & irq_getmask();
 
-	if(irqs & (1 << YM2151_INTERRUPT))
-	{
-		// Fun: Set timer A to 0x3FF and uncomment the below line.
-		//write_ym2151_wait(CT, CT_BITS(0x03));
-		unsigned int reason = ym2151_ev_pending_read();
-		if(reason & 0x01)
-		{
-			unsigned int timer_flags = readirq_timerab();
-			if(timer_flags & TIMER_A)
-			{
-				timera_ov = 1;
-			}
+    if(irqs & (1 << YM2151_INTERRUPT))
+    {
+        // Fun: Set timer A to 0x3FF and uncomment the below line.
+        //write_ym2151_wait(CT, CT_BITS(0x03));
+        unsigned int reason = ym2151_ev_pending_read();
+        if(reason & 0x01)
+        {
+            unsigned int timer_flags = readirq_timerab();
+            if(timer_flags & TIMER_A)
+            {
+                timera_ov = 1;
+            }
 
-			if(timer_flags & TIMER_B)
-			{
-				timerb_ov = 1;
-			}
+            if(timer_flags & TIMER_B)
+            {
+                timerb_ov = 1;
+            }
 
-			ctl_timerab(1, 1, 0, 0, timer_flags);
-			ym2151_ev_pending_write(0x01);
-		}
+            ctl_timerab(1, 1, 0, 0, timer_flags);
+            ym2151_ev_pending_write(0x01);
+        }
 
-		if(reason & 0x02)
-		{
-			handle_sample();
-			ym2151_ev_pending_write(0x02);
-		}
-	}
+        if(reason & 0x02)
+        {
+            handle_sample();
+            ym2151_ev_pending_write(0x02);
+        }
+    }
 
-	if(irqs & (1 << HOST_INTERRUPT))
-	{
-		unsigned int ctl = host_host_ctl_read();
-		if(ctl & 0x02) // Second bit: Data avail
-		{
-			unsigned short int size = host_host_size_read();
-			if(size == 4)
-			{
-				memcpy((unsigned char *) &host_flag, (unsigned char *) XFER_BASE + 256, 4);
-			}
-		}
-		host_ev_pending_write(0x01);
-	}
+    if(irqs & (1 << HOST_INTERRUPT))
+    {
+        unsigned int ctl = host_host_ctl_read();
+        if(ctl & 0x02) // Second bit: Data avail
+        {
+            unsigned short int size = host_host_size_read();
+            if(size == 4)
+            {
+                memcpy((unsigned char *) &host_flag, (unsigned char *) XFER_BASE + 256, 4);
+            }
+        }
+        host_ev_pending_write(0x01);
+    }
 }
 
 
 void handle_sample(void)
 {
-	num_samples++;
+    num_samples++;
 }
